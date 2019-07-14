07073d4b6c1f4b3d0c893c5d631c7f6994e36fde
arcadia168 <bennawaz168@icloud.com>
2019-07-14 15:38:01 +0100

    Adds function to library to retrieve details for a given Gist ID.

--- project/src/services/github-gist-api-service.js
+++ project/src/services/github-gist-api-service.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
+const GITHUB_GIST_API_URL = 'https://api.github.com';
 const getGistsForUser = username => {
     debugger;
 
-    return axios.get(`https://api.github.com/users/${username}/gists`
+    return axios.get(`${GITHUB_GIST_API_URL}/users/${username}/gists`
         ).then(response => {
         debugger;
         console.info(`response is: ${JSON.stringify(response)}`);
@@ -15,4 +16,19 @@ const getGistsForUser = username => {
     })
 }
 
-export default getGistsForUser;
+const getGist = gistId => {
+    debugger;
+    console.log(`Getting singular Gist info for ${gistId}`);
+    return axios.get(`${GITHUB_GIST_API_URL}/gists/${gistId}`
+        ).then(response => {
+        debugger;
+        console.info(`response is: ${JSON.stringify(response)}`);
+        return response;
+    }).catch(error => {
+        debugger;
+        console.error(`error is: ${JSON.stringify(error)}`);
+        throw error;
+    })
+}
+
+export { getGistsForUser, getGist };
