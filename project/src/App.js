459136d140afbe53bd25961a6fd5c24a48fcbb3c
Erin Eland <erin@erineland.com>
2019-07-14 15:24:11 +0100

    Added search box and hooked up to search button

--- project/src/App.js
+++ project/src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import getGistsForUser from './services/github-gist-api-service';
-import logo from './logo.svg';
 import './App.css';
-
-
 
 class App extends Component {
 
@@ -13,14 +9,23 @@ class App extends Component {
     this.state = {
       gistsToShow: [],
       error: undefined,
+      searchTerm: '',
     }
+  }
+
+  searchTermChanged = e => {
+    console.info(`Search term being updated to: ${e.target.value}`);
+    this.setState({
+      searchTerm: e.target.value,
+    })
   }
 
   getGistsForUserClicked = username => {
     debugger;
 
-    const testUser = 'arcadia168';
-    getGistsForUser(testUser).then(response => {
+    const userToSearch = this.state.searchTerm;
+    console.info(`The GitHub username search term is: ${userToSearch}`);
+    getGistsForUser(userToSearch).then(response => {
       debugger;
       console.info(`response is: ${JSON.stringify(response)}`);
       this.setState({
@@ -39,6 +44,7 @@ class App extends Component {
     return (
       <div className="gist-viewer__container">
         <div className="gist-viewer__gist-search-control-container">
+          <input onChange={this.searchTermChanged} type="text" placeholder="Enter GitHub username to retrieve thier public Gists..."/>
           <button onClick={this.getGistsForUserClicked} className="gist-viwer__search-btn">Search Gists</button>
         </div>
         <div className="gist-viewer__gists-search-results">
