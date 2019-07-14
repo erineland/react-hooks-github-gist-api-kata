c92ef41b7fe7b6858e37e9d142cdce82b12b8881
arcadia168 <bennawaz168@icloud.com>
2019-07-14 22:41:56 +0100

    Fixed a bug whereby state wasn't updated due to using an object

--- project/src/gistViewer.jsx
+++ project/src/gistViewer.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { getGistsForUser, getGist } from './services/github-gist-api-service';
 
-import GistDetails from './gistDetails';
+// import GistDetails from './gistDetails';
 import './App.css';
 
 const GistViewer = () => {
@@ -10,29 +10,57 @@ const GistViewer = () => {
   const [error, setError] = useState(undefined);
   const [searchTerm, setSearchTerm] = useState('');
   const [gistDetailsToShow, setGistDetailsToShow] = useState(undefined);
-  const [userFavourites, setUserFavourites] = useState({});
+  const [userFavourites, setUserFavourites] = useState([]);
   const [currentUsername, setCurrentUsername] = useState('');
 
-  const setFavouriteFile = (favouriteFilename) => {
-    debugger;
-    const username = currentUsername;
+  const isFileCurrentUserFavourite = (filename, currentUsername, userFavourites) => {
+    debugger
+    // Retreive favourite files for the current username
+    const currentUserFavourites = userFavourites.filter(currentUserFavourites => {
+      return currentUserFavourites.username === currentUsername;
+    });
 
+    // Now check to see if the file to be rendered is a favourite of the currently searched user.
+    let fileIsFavouriteOfCurrentUser = false;
+    if (currentUserFavourites.length > 0) {
+      fileIsFavouriteOfCurrentUser = currentUserFavourites[0].favourites.indexOf(filename) > -1;
+    }
+
+    return fileIsFavouriteOfCurrentUser
+  }
+
+  const setFavouriteFile = favouriteFilename => {
+    debugger;
     console.info(`Setting user favourite file in outer component scope`);
     console.info(`Setting user favourite file with filename: ${favouriteFilename}`);
-    console.info(`Setting user favourite file for user: ${username}`);
+    console.info(`Setting user favourite file for user: ${currentUsername}`);
 
-    const currentUserFavourites = userFavourites;
-    console.info(`Current userFavourites are: ${JSON.stringify(currentUserFavourites)}`);
-    const currentUserFavouriteFiles = currentUserFavourites[username];
+    console.info(`Current userFavourites are: ${JSON.stringify(userFavourites)}`);
 
-    if (currentUserFavourites[username]) {
-      currentUserFavourites[username] = currentUserFavouriteFiles.concat(favouriteFilename);
-    } else {
-      currentUserFavourites[username] = [favouriteFilename];
+    let userFavouritesFound = false;
+    for (let i = 0; i < userFavourites.length; i++) {
+      const currentUserFavourites = userFavourites[i];
+
+      if (currentUserFavourites.username === currentUsername) {
+        userFavouritesFound = true;
+
+        if (currentUserFavourites.favourites.indexOf(favouriteFilename) < -1) {
+          currentUserFavourites.favourites.push(favouriteFilename);
+        }
+      }
+    }
+
+    if (!userFavouritesFound) {
+      const newUserFavourites = {
+        username: currentUsername,
+        favourites: [favouriteFilename]
+      }
+
+      userFavourites.push(newUserFavourites);
     }
 
     // Now update the favourite files with the new favourite files...
-    setUserFavourites(currentUserFavourites);
+    setUserFavourites([...userFavourites]);
   }
 
   const searchTermChanged = e => {
@@ -81,12 +109,12 @@ const GistViewer = () => {
     });
   }
 
-  const gistDetails = {
-    setFavourite: setFavouriteFile,
-    gistDetails: gistDetailsToShow,
-    userFavourites: userFavourites,
-    currentUsername: currentUsername
-  }
+  // const gistDetails = {
+  //   setFavourite: setFavouriteFile,
+  //   gistDetails: gistDetailsToShow,
+  //   userFavourites: userFavourites,
+  //   currentUsername: currentUsername
+  // }
 
   return (
     <div className="gist-viewer__container">
@@ -94,13 +122,49 @@ const GistViewer = () => {
         <input onChange={searchTermChanged} type="text" placeholder="Enter GitHub username to retrieve thier public Gists..." />
         <button onClick={getGistsForUserClicked} className="gist-viwer__search-btn">Search Gists</button>
       </div>
+      <div>
+        <span>User favourites are: {JSON.stringify(userFavourites)}</span>
+      </div>
       {
         gistDetailsToShow ? //If a Gist has been clicked, show only the details of that Gist
           <div>
             <div>
               <button onClick={showAllGists}>Show all Gists for {currentUsername} (Back)</button>
             </div>
-            <GistDetails {...gistDetails} />
+            <div className="gist-details__container">
+              <h1>Gist Details!</h1>
+              <div>
+                <h2>Files in Gist:</h2>
+                {
+                  Object.keys(gistDetailsToShow.files).map((fileKey, index) => {
+                    const currentFileDetails = gistDetailsToShow.files[fileKey];
+                    console.info(
+                      `The current file details being rendered are: ${JSON.stringify(currentFileDetails)}`
+                    );
+                    return <div key={index}>
+                      <div className="gist-details__title-container">
+                        <h3>
+                          Filename: {currentFileDetails.filename}
+                          {
+                            isFileCurrentUserFavourite(
+                              currentFileDetails.filename,
+                              currentUsername,
+                              userFavourites,
+                            ) ? <span> - FAVOURITED! </span> : null
+                          }
+                        </h3>
+                        <button onClick={() => setFavouriteFile(currentFileDetails.filename)}>Favourite</button>
+                      </div>
+                      <div>
+                        <h4>Content:</h4>
+                        <code>{currentFileDetails.content}</code>
+                      </div>
+                    </div>
+                  })
+                }
+              </div>
+            </div>
+            );
           </div>
           :
           <div className="gist-viewer__gists-search-results">
