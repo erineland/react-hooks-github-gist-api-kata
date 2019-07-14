9eeed132caa8fa596be150cfccb90dec6a38f321
arcadia168 <bennawaz168@icloud.com>
2019-07-14 16:53:07 +0100

    Show if given file within given Gist for given user is favourited.

--- project/src/gistDetails.jsx
+++ project/src/gistDetails.jsx
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 
 class GistDetails extends Component {
-    constructor(props) {
-        super(props);
+    isFileCurrentUserFavourite = filename => {
+        debugger;
+        const currentUsername = this.props.currentUsername;
+        const userFavourites = this.props.userFavourites;
+
+        console.info(`The currentUsername: ${currentUsername}`);
+        console.info(`The current favourites are: ${JSON.stringify(userFavourites)}`);
+
+        // Retreive favourite files for the current username
+        const currentUserFavourites = userFavourites[currentUsername];
+        console.info(`The favourites for the current user ${currentUsername} are: ${currentUserFavourites}`);
+
+        // Now check to see if the file to be rendered is a favourite of the currently searched user.
+        let fileIsFavouriteOfCurrentUser = false;
+        if (currentUserFavourites) {
+            fileIsFavouriteOfCurrentUser = currentUserFavourites.indexOf(filename) > -1;
+        }
+
+        console.info(`The current file ${filename} is a favourite of the current user ${currentUsername}? ${fileIsFavouriteOfCurrentUser}`);
+        return fileIsFavouriteOfCurrentUser
     }
 
     render() {
@@ -21,7 +39,13 @@ class GistDetails extends Component {
                                 `The current file details being rendered are: ${JSON.stringify(currentFileDetails)}`
                             );
                             return <div key={index}>
-                                <h3>Filename: {currentFileDetails.filename}</h3>
+                                <h3>
+                                    Filename: {currentFileDetails.filename}
+                                    {
+                                        this.isFileCurrentUserFavourite(currentFileDetails.filename) ?
+                                            <span> - FAVOURITED! </span> : null
+                                    }
+                                </h3>
                                 <h4>Content:</h4>
                                 <p>{currentFileDetails.content}</p>
                                 <button onClick={() => setFavourite(currentFileDetails.filename)}>Mark file as favourite</button>
